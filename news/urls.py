eb19b36bc2d2c923eab45d662008051096550a3b
Islam <it.kaktus312@gmail.com>
2024-01-24 23:53:49 +0600

    Adding POST, PUT and DELETE requests

--- news/urls.py
+++ news/urls.py
@@ -3,8 +3,10 @@ from news import views
 
 urlpatterns = [
     path('api/v1/test/', views.hello_world),
-    path('api/v1/news/', views.news_list),
-    path('api/v1/news/<int:news_id>/', views.news_detail),
+
+    path('api/v1/news/', views.news_list),  # GET -> list, POST -> create
+    path('api/v1/news/<int:news_id>/', views.news_detail),  # GET -> retrieve or detail, PUT -> update, DELETE -> delete
+
 
     path('api/v1/comments/', views.comments_list),
 ]
--- news/views.py
+++ news/views.py
@@ -12,27 +12,66 @@ def hello_world(request):
     return Response(dct)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def news_list(request):
-    search = request.GET.get('search', '')
+    if request.method == 'GET':
+        search = request.GET.get('search', '')
 
-    # Get all news
-    news = News.objects.select_related('category').prefetch_related(
-            'tags', 'comments').filter(title__icontains=search)
-    # Serialize news
-    serializer = NewsListSerializer(instance=news, many=True)
-    print(serializer)
-    return Response(serializer.data)
+        # Get all news
+        news = News.objects.select_related('category').prefetch_related(
+                'tags', 'comments').filter(title__icontains=search)
+        # Serialize news
+        serializer = NewsListSerializer(instance=news, many=True)
+        print(serializer)
+        return Response(serializer.data, status=200)
+
+    elif request.method == 'POST':
+        title = request.data.get('title')
+        content = request.data.get('content')
+        category_id = request.data.get('category_id')
+        tags = request.data.get('tags', [])
+        news = News.objects.create(
+            title=title,
+            content=content,
+            category_id=category_id
+        )
+
+        # 1 способ
+        news.tags.set(tags)
+
+        # 2 способ
+        # for tag_id in tags:
+        #     news.tags.add(tag_id)
+
+        # 3 способ
+        # news.tags.add(*tags)
+
+        data = NewsListSerializer(instance=news, many=False).data
+        return Response(data, status=201)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def news_detail(request, news_id):
     try:
         news = News.objects.get(id=news_id)
     except News.DoesNotExist as e:
         return Response({'error': str(e)}, status=404)
-    serializer = NewsDetailSerializer(instance=news, many=False)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = NewsDetailSerializer(instance=news, many=False)
+        return Response(serializer.data, status=200)
+
+    elif request.method == 'PUT':
+        news.title = request.data.get('title', news.title)
+        news.content = request.data.get('content', news.content)
+        news.category_id = request.data.get('category_id', news.category_id)
+        news.tags.set(request.data.get('tags', news.tags.all()))
+        news.save()
+        data = NewsListSerializer(instance=news, many=False).data
+        return Response(data, status=200)
+
+    elif request.method == 'DELETE':
+        news.delete()
+        return Response(status=204)
 
 
 @api_view(['GET'])
